7d689760f55dcf9e26dfcbd1a0f8f64e6a23ee48
Sadya Hossain Sinthi <sadyasinthi1@gmail.com>
2025-05-04 22:53:41 -0400

    Ready for Deployment

---
+++ frontend/src/environment.js
@@ -0,0 +1,14 @@
+/*
+
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://apnacollegebackend.onrender.com" :
+
+    "http://localhost:8000";
+
+
+ export default server;*/
+ const server = "http://localhost:8000";
+
+ export default server;
+ 
